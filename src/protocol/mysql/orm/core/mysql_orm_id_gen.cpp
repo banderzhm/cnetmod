d8f02e982291dec0db4a8b390759f3b0976c7d65
250074249 <250074249@qq.com>
2026-07-21 20:17:48 +0800

    fix(windows): include Windows headers before bcrypt

--- src/protocol/mysql/orm/core/mysql_orm_id_gen.cpp
+++ src/protocol/mysql/orm/core/mysql_orm_id_gen.cpp
@@ -6,8 +6,8 @@ module;
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <bcrypt.h>
 #include <windows.h>
+#include <bcrypt.h>
 #pragma comment(lib, "bcrypt.lib")
 #endif
 
